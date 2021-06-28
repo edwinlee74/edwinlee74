779a68c931856cf153c1d1ef024853472fa73e57
Shao-Chi <edwinlee1974@gmail.com>
2021-06-28 19:14:56 +0800

    update crawl task

--- StockSituationRoom/crawl.py
+++ StockSituationRoom/crawl.py
@@ -1,15 +1,88 @@
+import requests
+import atexit
 from apscheduler.schedulers.background import BackgroundScheduler
+from bs4 import BeautifulSoup
+from datetime import date
 
-def crawl_tesk():
-    print("crawl tesk begin...")
+stock_company_list_url = 'https://isin.twse.com.tw/isin/C_public.jsp?strMode=2' 
+month_revenue_url = 'https://mops.twse.com.tw/nas/t21/sii/'
 
-def tesk2():
-    print("tesk2 starting...")
+def get_html(url: str) -> str:
+    try:
+        headers = {'user-agent': 'Mozilla/5.0'}
+        response = requests.get(url, headers=headers, timeout = 30)
+        response.raise_for_status()
+        response.encoding = response.apparent_encoding
+        return response.text
+    except:
+        return ""
+
+def parse_company_list():
+    html = get_html(stock_company_list_url)
+    soup = BeautifulSoup(html, 'html.parser')
+    index = 1        # An index of field value
+    row_dict = {} 
+    row_list = []
+
+    for item in soup.find_all('td')[8:-1000]:    
+
+         if not item.string: 
+            index += 1
+            continue
+
+         if index == 8:
+            row_list.append(row_dict)
+            row_dict = {}
+            index = 1
+            stock = item.string
+            stock_no = stock.split()[0]
+            company = stock.split()[1]
+            row_dict.update({'stock_no':stock_no})
+            row_dict.update({'company_name':company})
+         
+         if index == 1:
+            stock = item.string
+            stock_no = stock.split()[0]
+            company = stock.split()[1]
+            row_dict.update({'stock_no':stock_no})
+            row_dict.update({'company_name':company})
+
+         if index == 3:
+            date = item.string
+            row_dict.update({'listing_date':date})
+     
+         if index == 5:
+            category = item.string
+            row_dict.update({'category':category})
+
+         if item.string == '030001　萬海中信0B購01':
+            row_list.append(row_dict)
+            return row_list
+
+         index += 1
+ 
+def parse_company_revenue():
+    today = date.today()
+    year = str(today.year - 1911)              #轉成民國年號
+    month = str(today.month)
+    file = f't21sc03_{year}_{month}.csv'
+    url = month_revenue_url + file
+    csv_file = get_html(url)
+    for item in csv_file.splitlines()[1:]:
+       stock_no = item.split(',')[2]
+       revenue = item.split(',')[5]
+       print(stock_no, ' ', revenue)
+
+def grab_stock_name_tesk():
+    company_list = parse_company_list()
+    print(company_list)
+    return
+
+def get_company_revenue_task():
+    parse_company_revenue()
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=crawl_tesk, trigger='cron', minute='*/1', max_instances=3)
+scheduler.add_job(func=grab_stock_name_tesk, trigger='cron', minute='*/30')
+scheduler.add_job(func=get_company_revenue_task, trigger='cron', minute='*/2')
 scheduler.start()
-
-scheduler2 = BackgroundScheduler()
-scheduler2.add_job(func=tesk2, trigger='cron', minute='*/2')
-scheduler2.start()
+atexit.register(lambda: scheduler.shutdown())
--- StockSituationRoom/model.py
+++ StockSituationRoom/model.py
@@ -8,3 +8,23 @@ class Company(db.Model):
     
     def __repr__(self) -> str:
         return '<Company %r %r>' %(self.stock_no, self.company_name)
+
+class MonthRevenue(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    year = db.Column(db.Integer)
+    january = db.Column(db.Integer)
+    february = db.Column(db.Integer)
+    march = db.Column(db.Integer)
+    april = db.Column(db.Integer)
+    may = db.Column(db.Integer)
+    june = db.Column(db.Integer)
+    july = db.Column(db.Integer)
+    august = db.Column(db.Integer)
+    september = db.Column(db.Integer)
+    october = db.Column(db.Integer)
+    november = db.Column(db.Integer)
+    december = db.Column(db.Integer)
+    stock_no = db.Column(db.String(8), db.ForeignKey('company.stock_no'))
+
+    def __repr__(self) -> str:
+        return '<MonthRevenue %r>' % self.stock_no
